c1590ab53e70f373b8fe9e3cb164db9bfac737e1
Michela Tsope <93129619+mtsope@users.noreply.github.com>
2022-08-02 11:56:56 +0200

    First topic

---
+++ first-react/index.js
@@ -0,0 +1,29 @@
+
+
+const navbar = (
+    <nav>
+        <h1>Website</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Conact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
+
+// const page = (
+//     <div>
+//         <h1 className="header">This is my firs react lesson</h1>
+//         <p>Those are my lessons</p>
+//     </div>
+// )
+// ReactDOM.render(page , document.getElementById("root"))
+
+// /* <ul>
+//             <li>HTML</li>
+//             <li>CSS</li>
+//             <li>Bootstrap</li>
+//             <li>Jquery</li>
+//         </ul> */
